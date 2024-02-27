473a90b528bc0eb22b28315a28a7cf99ac021e9b
park <minpark93@gmail.com>
2024-02-26 16:35:46 -0800

    testing

--- src/components/GetRandomDog.jsx
+++ src/components/GetRandomDog.jsx
@@ -1,14 +1,22 @@
 
 const GetRandomDog = ({ example }) => {
     // maximum of 3 images
-    if (example.length >= 4) {
-        example = example.splice(0, 1)
-    }
+    // if (example.length >= 4) {
+    //     example = example.splice(0, 1)
+    // }
+    const limitExample = example.slice(0, 3)
     return(
         <div className='randomDog'>
-            <img src={`${example[0]}`} style={{width: '200px', height: '150px'}}/>
+            {
+                limitExample.map((image, index) => {
+                    return(
+                        <img src={`${image}`} key={index} style={{width: '200px', height: '150px'}}/>
+                    )
+                })
+            }
+            {/* <img src={`${example[0]}`} style={{width: '200px', height: '150px'}}/>
             <img src={`${example[1]}`} style={{width: '200px'}}/>
-            <img src={`${example[2]}`} style={{width: '200px'}}/>
+            <img src={`${example[2]}`} style={{width: '200px'}}/> */}
         </div>
     )
 }
